91a5185734abc66a5044e0d54b76b120835ad036
Duvan Villadiego <duvan.villadiego.oficial@gmail.com>
2021-07-24 22:52:14 -0500

    feat: architecture instance

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const {dbConnection} = require("./db/db")
+const sequelize = require("./db/db")
 require ("dotenv").config()
 
 const app = express()
@@ -9,6 +9,4 @@ app.use(express.json())
 //endpoints
 
 
-app.listen(process.env.PORT, () => {console.log("Backend server listening on port" + process.env.PORT)})
-
-dbConnection()
+app.listen(process.env.PORT, () => {console.log("Backend server listening on port " + process.env.PORT)})
