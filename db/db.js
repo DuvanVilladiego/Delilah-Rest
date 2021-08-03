9370baba5b47b18346eb98d7ba02c9ce0ea23b94
Mario Jose Rodriguez Machacon <75395682+mrodriguezm21@users.noreply.github.com>
2021-08-02 23:02:48 -0500

    middleware 0.2

--- db/db.js
+++ db/db.js
@@ -1,17 +1,17 @@
-const Sequelize = require('sequelize');
-const path = process.env.PATH;
+const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(path, {
-    operatorsAliases: false ,
-    logging: true,
-});
+const sequelize = new Sequelize(
+  "mariadb://root:admin@localhost:3306/delilah_resto"
+);
 
-const dbConnection = async () => {
-    sequelize.authenticate().then(() => {
-        console.log('Conectado.');
-    }).catch(err => {
-        console.error('Error de conexion:', err);
+    sequelize
+    .authenticate()
+    .then(() => {
+      console.log("Conectado.");
+    })
+    .catch((err) => {
+      console.error("Error de conexion:", err);
     });
-}
 
-module.exports = {dbConnection}
+
+module.exports = sequelize
--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-const express = require("express")
-const {dbConnection} = require("./db/db")
-require ("dotenv").config()
+const express = require("express");
+const sequelize = require("./db/db");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+async function findUsername() {
+  sequelize
+    .query("SELECT username FROM usuario where username ='pedrito123'", {
+      type: sequelize.QueryTypes.SELECT,
+    })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((e) => {
+      console.log("error  en la insercion " + e);
+    });
+}
+
+app.use(express.json());
 
 //endpoints
+app.get("/", (req, res) => {
+    console.log(findUsername());
+  if (findUsername() == [] || !findUsername()) {
+   return res.status(400).json({msg: "Usuario existente!"});
+  }
+  else{
+    res.status(200).json({msg: "Usuario created"});
 
+  }
+});
 
-app.listen(process.env.PORT, () => {console.log("Backend server listening on port" + process.env.PORT)})
-
-dbConnection()
+app.listen(process.env.PORT, () => {
+  console.log("Backend server listening on port " + process.env.PORT);
+});
